dd0e94e839e40a563399d55e9939d1fc0aec587e
jim.shelly <jim.shelly@gmail.com>
2020-01-02 20:50:09 -0600

    added media schema and updated the GET route to pull from the database.

--- server/routes/media.js
+++ server/routes/media.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var MediaModel = require('../models/media');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
     //change this code to be json and res.send rather than res.render
-    res.send({ "media": "success" });
+    try {
+
+        var media = await MediaModel.find();
+        res.json(media);
+    } catch (err) {
+        console.log('Error: ', err);
+    }
+
 });
 
 router.post('/', function (req, res, next) {
