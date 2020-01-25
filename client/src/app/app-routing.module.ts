ab85dcf5442041bfb26bb33045247b92da97e0b8
jim.shelly <jim.shelly@gmail.com>
2020-01-24 19:37:13 -0600

    Merged from main fork

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { CreatemediaComponent } from "./createmedia/createmedia.component";
 import { MediaComponent } from "./media/media.component";
+import { ContactComponent } from "./contact/contact.component";
 
 const routes: Routes = [
+  { path: "", component: MediaComponent },
   { path: "create", component: CreatemediaComponent },
-  { path: "list", component: MediaComponent }
+
+  { path: "contact", component: ContactComponent }
 ];
 
 @NgModule({
